b5c5dbecb2df4406fad1bef3d4234fc76e49875f
Zico_Slayer <59080118+itthikorn2542@users.noreply.github.com>
2020-11-24 00:07:19 +0700

    delete Store

--- Store.js
+++
@@ -1,7 +0,0 @@
-import SongReducer from './Reducer/SongReducer'
-import {createStore,combineReducers} from 'redux'
-const rootReducer = combineReducers({
-  SongReducer:SongReducer
-})
-const configureStore = createStore(rootReducer);
-export default configureStore;
